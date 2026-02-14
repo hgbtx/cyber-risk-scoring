2893dd886cdd33ae67e3783259516a4181eac5c0
hgbtx <mojavegravy@gmail.com>
2026-02-13 17:11:18 -0800

    Updating Charts tab and Left Panel features

--- static/app.js
+++ static/app.js
@@ -1388,6 +1388,8 @@ document.getElementById('riskThresholdSlider').addEventListener('input', (e) => 
     renderCiaRadarChart();
 });
 
+document.getElementById('chartConfig').style.display = 'none';
+
 // =====================
 // SEARCH RESULT FILTERS
 // =====================
